d276d9cd742b094a174878e3764bd7fcf0929c04
khalil <khalilfarashi2000@gmail.com>
2024-05-02 19:09:16 +0330

    feat: Add repository section
    Add mysql config and load func in repository

---
+++ wallet/internal/repository/mysql/db.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type Config struct {
+	Username string `koanf:"username"`
+	Password string `koanf:"password"`
+	Port     int    `koanf:"port"`
+	Host     string `koanf:"host"`
+	DBName   string `koanf:"db_name"`
+}
+
+type MySQLDB struct {
+	config Config
+	db     *sql.DB
+}
+
+func (m *MySQLDB) Conn() *sql.DB {
+	return m.db
+}
+
+func New(config Config) *MySQLDB {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s:%d)/%s?parseTime=true",
+		config.Username, config.Password, config.Host, config.Port, config.DBName))
+	if err != nil {
+		panic(fmt.Errorf("can't open mysql db: %v", err))
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return &MySQLDB{config: config, db: db}
+}
