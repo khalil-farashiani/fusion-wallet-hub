3df90198752ea3a2602fe8c9a7beb00c79acc321
khalil <khalilfarashi2000@gmail.com>
2024-05-04 01:32:54 +0330

    refactor: Change the type of transaction entity
    Use iota and seperate type field for transaction type

--- wallet/internal/transaction/entity/transaction.go
+++ wallet/internal/transaction/entity/transaction.go
@@ -2,11 +2,32 @@ package entity
 
 import "time"
 
+type TransactionType int
+
+const (
+	Debit  TransactionType = iota // 0
+	Credit                        // 1
+)
+
+// String returns the string representation of the TransactionType.
+func (t TransactionType) String() string {
+	types := []string{
+		"DEBIT",
+		"CREDIT",
+	}
+	if int(t) < len(types) {
+		return types[t]
+	}
+
+	//TODO: handle invalid types gracefully
+	return "UNKNOWN"
+}
+
 type Transaction struct {
-	ID        uint   `json:"id"`
-	AccountID string `json:"account_id"`
-	Type      string `json:"type"`
-	Amount    uint64 `json:"amount"`
+	ID        uint            `json:"id"`
+	AccountID string          `json:"account_id"`
+	Type      TransactionType `json:"type"`
+	Amount    uint64          `json:"amount"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
