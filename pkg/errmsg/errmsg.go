a861116d84476fe499c96a7863a10c0616f4296b
khalil <khalilfarashi2000@gmail.com>
2024-05-03 13:13:52 +0330

    feat: Add error message package
    Containes error message const

---
+++ pkg/errmsg/errmsg.go
@@ -0,0 +1,12 @@
+package errmsg
+
+const (
+	ErrorMsgNotFound               = "record not found"
+	ErrorMsgCantScanQueryResult    = "can't scan query result"
+	ErrorMsgSomethingWentWrong     = "something went wrong"
+	ErrorMsgPhoneNumberIsNotUnique = "phone number is not unique"
+	ErrorMsgInvalidInput           = "invalid input"
+	ErrorMsgPhoneNumberIsNotValid  = "phone number is not valid"
+	ErrorMsgUserNotAllowed         = "user not allowed"
+	ErrorMsgCategoryIsNotValid     = "category is not valid"
+)
