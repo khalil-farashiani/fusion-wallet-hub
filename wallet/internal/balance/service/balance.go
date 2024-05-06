e60f356572f9fea539cda1ece1b0325a0f8df45f
khalil <khalilfarashi2000@gmail.com>
2024-05-07 02:08:44 +0330

    feat: Add balance service
    We have one service  GetUserBalance

--- wallet/internal/balance/service/balance.go
+++ wallet/internal/balance/service/balance.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/khalil-farashiani/fusion-wallet-hub/pkg/richerror"
 	"github.com/khalil-farashiani/fusion-wallet-hub/wallet/internal/balance/entity"
 )
 
@@ -14,4 +15,17 @@ type Service struct {
 
 func New(repo Repository) Service {
 	return Service{repo: repo}
+}
+
+func (s Service) GetUserBalance(userID string) (entity.Balance, error) {
+	const op = "service.GetUserBalance"
+
+	balance, err := s.repo.GetUserBalance(userID)
+	if err != nil {
+		//might better sanitize request
+		return entity.Balance{},
+			richerror.New(op).WithErr(err).WithKind(richerror.Unexpected)
+	}
+
+	return balance, err
 }
